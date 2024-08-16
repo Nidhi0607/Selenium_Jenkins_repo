015d4f0262228261fd500649611929f6fa5991fb
saketlahariya <saketlahariya@localhost>
2024-08-16 11:50:55 +0530

    Updated pollscm file

---
+++ JnkinsSession/src/test/java/Jenkins/PollScmTest.java
@@ -0,0 +1,11 @@
+package Jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollScmTest {
+	@Test
+	public void poll() {
+		Reporter.log("Poll scm calss added and executed",true);
+	}
+}
